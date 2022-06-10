a9be082f56c1a0ddf9ce4bcb30b471cb2cca802a
Vee Uye <vee.uye@gmail.com>
2022-06-10 22:17:09 +0100

    Render real images in Search Results component and style

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,10 +3,22 @@ import "../styles/search-results.css";
 import testImage from "../assets/images/test-image.jpeg";
 
 const SearchResults = ({ results }) => {
+  const images = results?.map((image, index) => (
+    <img
+      key={index}
+      className="search-results-image"
+      src={image}
+      alt="search results"
+    ></img>
+  ));
   return results && results.length > 0 ? (
     <>
       <p className="search-results-text">Search Results</p>
-      <img className="search-result" src={testImage} alt="test-result"></img>
+      <div className="search-results-wrap">
+        <div className="search-results">{images}</div>
+      </div>
+
+      {/* <img className="search-result" src={testImage} alt="test result"></img> */}
     </>
   ) : (
     <p className="search-results-text">No results</p>

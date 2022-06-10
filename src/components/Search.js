a881ca61868a17a4f834a1c40b07e9e00160624d
Vee Uye <vee.uye@gmail.com>
2022-06-10 14:44:28 +0100

    Add event handler to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "../styles/search.css";
 
 const Search = () => {
+  const [value, setValue] = useState();
+
   return (
     <>
       <form className="search-form">
-        <input className="search-input" type="text" placeholder="moon" />
+        <input
+          className="search-input"
+          type="text"
+          placeholder="moon"
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button
           className="search-button"
           type="submit"
